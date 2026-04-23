6915225a8ada4a4432d46214d12fb7385bb90f6e
leopard618 <snowleo1342@gmail.com>
2026-04-24 04:30:48 +0900

    section 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Blog from "@/components/Blog";
 import Footer from "@/components/Footer";
 import Intro from "@/components/Intro";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       <Intro />
+      <Services />
       <Blog />
       <Footer />
     </main>
